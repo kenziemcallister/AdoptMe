dc067d3d1f82983afd93259942029c1294211a37
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-05-04 02:01:27 -0500

    Javadoc for Cat Class
    Completed and Javadoc for cat class. Auto-set the type to "Cat" for the
    cat class.

--- src/main/java/edu/mu/adopt/model/pet/Cat.java
+++ src/main/java/edu/mu/adopt/model/pet/Cat.java
@@ -2,8 +2,21 @@ package edu.mu.adopt.model.pet;
 
 public class Cat extends Pet{
 
+	/* Default constructor for the Cat class
+	 * @param id (String)
+	 *     This argument represents the id of the cat.
+	 * @param name (String)
+	 *     This argument represents the name of the cat.
+	 * @param type (String)
+	 *     This argument represents the cat class.
+	 * @param species (String)
+	 *     This argument represents the species of the cat.
+	 * @param age (int)
+	 *     This argument represents the age of the cat.
+	 * */
 	public Cat(String id, String name, String type, String species, int age) {
 		super(id, name, type, species, age);
+		this.type = "Cat";
 	}
 
 }
