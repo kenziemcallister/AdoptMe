55a0e2fbabc90b83e54db81d0fed63bf5d79ce31
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-05-04 02:04:15 -0500

    Javadoc for Rabbit Class
    Auto-add to  default constructor "Rabbit" as type.

--- src/main/java/edu/mu/adopt/model/pet/Rabbit.java
+++ src/main/java/edu/mu/adopt/model/pet/Rabbit.java
@@ -2,8 +2,21 @@ package edu.mu.adopt.model.pet;
 
 public class Rabbit extends Pet {
 
+	/* Default constructor for the Rabbit class
+	 * @param id (String)
+	 *     This argument represents the id of the rabbit.
+	 * @param name (String)
+	 *     This argument represents the name of the rabbit.
+	 * @param type (String)
+	 *     This argument represents the rabbit class.
+	 * @param species (String)
+	 *     This argument represents the species of the rabbit.
+	 * @param age (int)
+	 *     This argument represents the age of the rabbit.
+	 * */
 	public Rabbit(String id, String name, String type, String species, int age) {
-		super(id, name, type, species, age);		
+		super(id, name, type, species, age);	
+		this.type = "Rabbit";
 	}
 	
 	
