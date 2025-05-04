2742ecfc5a550c81386371f51b633f38fd72980e
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-05-04 02:02:43 -0500

    Completed Dog Javadoc
    Changed the default constructor to set type as "Dog".

--- src/main/java/edu/mu/adopt/model/pet/Dog.java
+++ src/main/java/edu/mu/adopt/model/pet/Dog.java
@@ -2,8 +2,21 @@ package edu.mu.adopt.model.pet;
 
 public class Dog extends Pet {
 
+	/* Default constructor for the Dog class
+	 * @param id (String)
+	 *     This argument represents the id of the dog.
+	 * @param name (String)
+	 *     This argument represents the name of the dog.
+	 * @param type (String)
+	 *     This argument represents the dog class.
+	 * @param species (String)
+	 *     This argument represents the species of the dog.
+	 * @param age (int)
+	 *     This argument represents the age of the dog.
+	 * */
 	public Dog(String id, String name, String type, String species, int age) {
 		super(id, name, type, species, age);
+		this.type = "Dog";
 	}
 
 }
